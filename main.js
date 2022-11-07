610bc477efbefc4ea1ae274773d3847a874063de
Pete982 <84607563+Pete982@users.noreply.github.com>
2022-11-07 08:24:29 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+
+
+
+
+setup(){
+    
+    video = createCapture(VIDEO);
+    
+    canvas = createCanvas(300, 300);
+  
+    video.size(300, 300);
+
+    video.position(10, 50);
+
+     canvas.position(430, 133);
+
+    length_results.on(pose, gotposes, on);
+
+    posenet = ml5.posenet(video, modelDone);
+
+    posenet.on("poses", gotPoses);
+
+
+}
+
+
+  function modelDone(){
+
+   console.log("posenet is ready and available!")
+
+}
+
+function draw(){
+
+    background("#ee1128");
+}
+
+function gotPoses( results, error ){
+
+     if(error){
+         
+        console.error(error);
+ 
+     }
+
+     if(results.length > 0){
+
+    console.log(results);
+
+        console.log("The rightWrist_x is "+ results[0].pose.rightWrist.x + "The RightWrist_y is"+ results[0].pose.rightWrist.y );
+        
+        console.log("The leftWrist_x is"+ results[0].pose.leftWrist.x+ "The LeftWrist_y is " + results[0].pose.leftWrist.y ) ;
+}
+
